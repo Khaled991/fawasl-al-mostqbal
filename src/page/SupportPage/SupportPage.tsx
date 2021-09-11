41ef71355739f9a436a36557a481ea5df701fa00
Khaled991 <khaled.waleed5357@gmail.com>
2021-09-11 16:54:36 +0200

    support page to grid responsive

--- src/page/SupportPage/SupportPage.tsx
+++ src/page/SupportPage/SupportPage.tsx
@@ -2,6 +2,31 @@ import { ReactElement } from 'react';
 import './SupportPage.scss';
 
 const SupportPage = (): ReactElement => {
+  const questions = Array.from({ length: 100 }).map(() => 'طريقة التثبيت');
+
+  const RenderQuestions = (): ReactElement[] => {
+    const questionsBlocks = [];
+    for (let i = 0; i < questions.length; i += 3) {
+      const questionsBlockArray = [];
+      for (let j = i; j < i + 3; j++) {
+        const questionElement = renderQuestion(questions[j]);
+        questionsBlockArray.push(questionElement);
+      }
+      const questionsBlockElement = renderQuestionsBlock(questionsBlockArray);
+      questionsBlocks.push(questionsBlockElement);
+    }
+
+    function renderQuestion(questionText: string): ReactElement {
+      return <span className="questions-text">{questionText}</span>;
+    }
+
+    function renderQuestionsBlock(questionsElements: ReactElement[]) {
+      return <div>{questionsElements}</div>;
+    }
+
+    return questionsBlocks;
+  };
+
   return (
     <div className="support-page">
       <div className="popular-question-text">
@@ -14,223 +39,11 @@ const SupportPage = (): ReactElement => {
           <div className="title-black-border" />
         </div>
       </div>
-      <div className="questions-container">
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
-        <span className="questions-text">- طريقة التثبيت</span>
+      <div className="questions-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {/* {Array.from({ length: 100 }).map(() => (
+          <span className="questions-text">- طريقة التثبيت</span>
+        ))} */}
+        {RenderQuestions}
       </div>
     </div>
   );
