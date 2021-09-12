7d5a3439eb36b207e3922337cc30abcb41c118ab
ahmed9190 <hamadawaled11@gmail.com>
2021-09-13 00:02:25 +0200

    change title and create loading pulse

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import Loading from "./components/loading/loading";
 import "./App.scss";
 
 const SupportPage = lazy(() => import("./page/SupportPage/SupportPage"));
@@ -11,14 +12,14 @@ const App = (): ReactElement => {
     <BrowserRouter>
       <div className="absolute">
         <NavBar />
-        <div className="z-1">
-          <Switch>
-            <Suspense fallback={<div>loading...</div>}>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/support" component={SupportPage} />
-            </Suspense>
-          </Switch>
-        </div>
+        {/* <div className="z-1"> */}
+        <Switch>
+          <Suspense fallback={<Loading />}>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/support" component={SupportPage} />
+          </Suspense>
+        </Switch>
+        {/* </div> */}
       </div>
     </BrowserRouter>
   );
